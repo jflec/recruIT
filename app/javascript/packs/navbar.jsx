b365a8acb26e286744528638bf09e8ac00e73cb4
JFlec <josephflec@gmail.com>
2021-12-13 16:24:31 -0600

    react

---
+++ app/javascript/packs/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div id="navbar-container">
+      <div id="navbar">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

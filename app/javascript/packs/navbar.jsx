664917b9ae0e0175f5f838c39a71ed617b543593
JFlec <josephflec@gmail.com>
2021-12-13 16:54:14 -0600

    css

--- app/javascript/packs/navbar.jsx
+++ app/javascript/packs/navbar.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div id="navbar-container">
-      <div id="navbar">
-        <h1>hello</h1>
+      <div id="navbar"></div>
+      <div id="nav-auth">
+        <h1 id="nav-login">Login</h1>
+        <h1 id="nav-signup">Sign Up</h1>
       </div>
     </div>
   );

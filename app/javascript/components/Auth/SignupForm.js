265217f0628842cd99aae9a4611961d7b182d33f
JFlec <josephflec@gmail.com>
2021-12-24 17:02:22 -0600

    christmas

--- app/javascript/components/Auth/SignupForm.js
+++ app/javascript/components/Auth/SignupForm.js
@@ -3,52 +3,44 @@ import React from 'react';
 const SignupForm = ({ handleChange, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Username
+      <div id="signup-form-one">
         <input
           type="text"
           placeholder="Username"
           name="username"
           onChange={handleChange}
         ></input>
-      </label>
-      <label>
-        Password
+
         <input
           type="password"
           placeholder="Password"
           name="password"
           onChange={handleChange}
         ></input>
-      </label>
-      <label>
-        Email
+
         <input
           type="text"
           placeholder="Email"
           name="email"
           onChange={handleChange}
         ></input>
-      </label>
-      <label>
-        First Name
+
         <input
           type="text"
           placeholder="First Name"
           name="first_name"
           onChange={handleChange}
         ></input>
-      </label>
-      <label>
-        Last Name
+
         <input
           type="text"
           placeholder="Last Name"
           name="last_name"
           onChange={handleChange}
         ></input>
-      </label>
-      <button type="submit">SIGN UP</button>
+
+        <button type="submit">SIGN UP</button>
+      </div>
     </form>
   );
 };

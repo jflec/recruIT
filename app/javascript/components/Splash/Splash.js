94cdb37720be5c29afb216d07d1ad798f0772e78
JFlec <josephflec@gmail.com>
2021-12-27 22:05:23 -0600

    commit

--- app/javascript/components/Splash/Splash.js
+++ app/javascript/components/Splash/Splash.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Splash = () => {
   return (
@@ -10,10 +11,12 @@ const Splash = () => {
       <div id="splash">
         <h1>Real connections real fast</h1>
         <p>designed by software engineers, for software engineers</p>
-        <button>
-          Register{' '}
-          <FontAwesomeIcon className="nav-right-arrow" icon={faArrowRight} />
-        </button>
+        <Link to={'/signup'}>
+          <button>
+            Register{' '}
+            <FontAwesomeIcon className="nav-right-arrow" icon={faArrowRight} />
+          </button>
+        </Link>
       </div>
     </div>
   );
